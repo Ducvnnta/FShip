39dad8b103386f1354f69c374b00d17f03959843
DucvnNTA <ducvn.nta@gmail.com>
2022-12-12 16:55:58 +0700

    Update/12/12

--- app/Repositories/User/UserRepositoryInterface.php
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -2,11 +2,15 @@
 
 namespace App\Repositories\User;
 
+use App\Models\User;
 use App\Repositories\BaseRepositoryInterface;
 
 interface UserRepositoryInterface extends BaseRepositoryInterface
 {
     public function getListAdminUser();
 
+    public function getPaginateSortId($perPage, $page, $columns = ['*']);
+
+    public function findByEmail($email): ?User;
 
 }
