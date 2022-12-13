999982be6e8115c48404f3814fc105a98758213b
DucvnNTA <ducvn.nta@gmail.com>
2022-12-13 16:57:54 +0700

    Update/13/12

--- app/Http/Controllers/API/V1/AuthController.php
+++ app/Http/Controllers/API/V1/AuthController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterUserRequest;
+use App\Repositories\User\UserRepositoryInterface;
 use App\Services\User\UserServiceInterface;
 use App\Traits\ApiResponser;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -15,14 +17,21 @@ class AuthController extends Controller
     /**
      * @var $userService
      */
+    protected $userRepository;
     protected $userService;
 
     public function __construct(
+        UserRepositoryInterface $userRepository,
         UserServiceInterface $userService
     ) {
-        $this->userService= $userService;
+        $this->userRepository = $userRepository;
+        $this->userService = $userService;
     }
 
+    public function registration()
+    {
+        return view('welcome');
+    }
 
     /**
      * Register user by email
@@ -31,7 +40,6 @@ class AuthController extends Controller
     public function register(RegisterUserRequest $request)
     {
         $result = $this->userService->registerUser($request);
-
         if (!$result) {
             $message = 'Unable To Register User.';
             $statusCode = Response::HTTP_BAD_REQUEST;
@@ -39,10 +47,10 @@ class AuthController extends Controller
             return $this->errorResponse($message, $statusCode);
         }
 
-        $data = [];
         $message = 'Successfully register';
         $statusCode = Response::HTTP_OK;
 
-        return $this->successResponse($data, $message, $statusCode);
+        return back()->with('success', 'User created successfully.');
+        // return $this->successResponse($result, $message, $statusCode);
     }
 }
